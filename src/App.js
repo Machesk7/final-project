231d14bd5b2ce4fec1f390a3a53e01a2197877e0
Jack Machesky <machesk7@msu.edu>
2023-05-01 00:25:12 -0400

    updatedd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,61 @@
+
+import React, { useState, useEffect } from 'react';
 import {supabase} from './supabaseclient';
 import logo from './logo.svg';
 import './App.css';
 import { SupabaseClient } from '@supabase/supabase-js';
 
+function Library() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data, error } = await supabase
+        .from('UCL')
+        .select('Team, Mascot, Points')
+        .order('id', { ascending: true });
+      if (error) {
+        console.log(error);
+      } else {
+        setData(data);
+      }
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Team</th>
+            <th>Mascot</th>
+            <th>Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row) => (
+            <tr key={row.id}>
+              <td>{row.Team}</td>
+              <td>{row.Mascot}</td>
+              <td>{row.Points}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Library />
+       
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
@@ -17,7 +63,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         
+          
         </a>
       </header>
     </div>
